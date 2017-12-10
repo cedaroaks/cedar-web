cfb40bdb536ea026105fe925e43cd19ac2c279aa
root <root@vmi137922.contaboserver.net>
2017-12-10 07:34:19 +0100

    Update 2017-12-10T06:34:15.577Z

--- js/app.08bb0aa3.js
+++ js/app.08bb0aa3.js
@@ -528,98 +528,6 @@
     'use strict';
     //@todo Move this service to the common folder
     angular.module('selfService')
-        .service('LoanAccountService', ['$q', '$http', '$rootScope', '$resource', 'BASE_URL', LoanAccountService]);
-
-    function LoanAccountService($q, $http, $rootScope, $resource, BASE_URL) {
-
-        this.loanAccount = function () {
-            return $resource(BASE_URL + '/self/loans/:id',{id: '@id'});
-        }
-    }
-
-})();
-
-
-(function () {
-    'use strict';
-
-    angular.module('selfService')
-        .controller('LoanAccountViewCtrl', ['$state', '$stateParams', '$filter', 'LoanAccountService', LoanAccountViewCtrl]);
-
-    /**
-     * @module LoanAccountViewCtrl
-     * @description
-     * Handles the Loan Account Details Page.
-     */
-    function LoanAccountViewCtrl($state, $stateParams, $filter, LoanAccountService) {
-
-        var vm = this;
-
-        /**
-         * @name loadingLoanAccountInfo
-         * @description flag to check whether account info is loaded or not
-         * @type {boolean}
-         */
-        vm.loadingLoanAccountInfo = true;
-
-        /**
-         * @name loanAccountDetails
-         * @type {object}
-         * @description To store the loan Account details returned by server
-         */
-        vm.loanAccountDetails = getLoanDetails($stateParams.id);
-
-        /**
-         * @name statusClass
-         * @type {string}
-         * @description To store the css class for loan status [active, pending, ...]
-         */
-        vm.statusClass = '';
-
-        vm.repaymentSchedule = {};
-
-        vm.makePayment = makePayment;
-
-        /**
-         * @method getLoanDetails
-         * @description To get the loan details from the server
-         * @param id {number} Loan Account id
-         */
-        function getLoanDetails(id) {
-            LoanAccountService.loanAccount().get({
-                id: id,
-                associations:'repaymentSchedule,transactions'
-            }).$promise.then(function (res) {
-                vm.loadingLoanAccountInfo = false;
-                vm.loanAccountDetails = res;
-                getStatusClass();
-            });
-        }
-
-        /**
-         * @method getStatusClass
-         * @description To get the loan account status through the status lookup filter
-         */
-        function getStatusClass() {
-            var statusClass = $filter('StatusLookup')(vm.loanAccountDetails.status.code);
-            statusClass = 'bg_' + statusClass;
-            if (vm.loanAccountDetails.inArrears) {
-                statusClass += 'overdue';
-            }
-            vm.statusClass = statusClass;
-        }
-
-        function makePayment() {
-            $state.go('app.transfers', {
-                toAccount: vm.loanAccountDetails
-            });
-        }
-    }
-})();
-(function () {
-    'use strict';
-    //@todo Move this service to the common folder
-    angular.module('selfService')
         .service('AccountService', ['$q', '$http', '$rootScope', '$resource', 'BASE_URL', 'storageService', AccountService]);
 
     function AccountService($q, $http, $rootScope, $resource, BASE_URL, storageService) {
@@ -736,6 +644,98 @@
 
 })();
 
+(function () {
+    'use strict';
+    //@todo Move this service to the common folder
+    angular.module('selfService')
+        .service('LoanAccountService', ['$q', '$http', '$rootScope', '$resource', 'BASE_URL', LoanAccountService]);
+
+    function LoanAccountService($q, $http, $rootScope, $resource, BASE_URL) {
+
+        this.loanAccount = function () {
+            return $resource(BASE_URL + '/self/loans/:id',{id: '@id'});
+        }
+    }
+
+})();
+
+
+(function () {
+    'use strict';
+
+    angular.module('selfService')
+        .controller('LoanAccountViewCtrl', ['$state', '$stateParams', '$filter', 'LoanAccountService', LoanAccountViewCtrl]);
+
+    /**
+     * @module LoanAccountViewCtrl
+     * @description
+     * Handles the Loan Account Details Page.
+     */
+    function LoanAccountViewCtrl($state, $stateParams, $filter, LoanAccountService) {
+
+        var vm = this;
+
+        /**
+         * @name loadingLoanAccountInfo
+         * @description flag to check whether account info is loaded or not
+         * @type {boolean}
+         */
+        vm.loadingLoanAccountInfo = true;
+
+        /**
+         * @name loanAccountDetails
+         * @type {object}
+         * @description To store the loan Account details returned by server
+         */
+        vm.loanAccountDetails = getLoanDetails($stateParams.id);
+
+        /**
+         * @name statusClass
+         * @type {string}
+         * @description To store the css class for loan status [active, pending, ...]
+         */
+        vm.statusClass = '';
+
+        vm.repaymentSchedule = {};
+
+        vm.makePayment = makePayment;
+
+        /**
+         * @method getLoanDetails
+         * @description To get the loan details from the server
+         * @param id {number} Loan Account id
+         */
+        function getLoanDetails(id) {
+            LoanAccountService.loanAccount().get({
+                id: id,
+                associations:'repaymentSchedule,transactions'
+            }).$promise.then(function (res) {
+                vm.loadingLoanAccountInfo = false;
+                vm.loanAccountDetails = res;
+                getStatusClass();
+            });
+        }
+
+        /**
+         * @method getStatusClass
+         * @description To get the loan account status through the status lookup filter
+         */
+        function getStatusClass() {
+            var statusClass = $filter('StatusLookup')(vm.loanAccountDetails.status.code);
+            statusClass = 'bg_' + statusClass;
+            if (vm.loanAccountDetails.inArrears) {
+                statusClass += 'overdue';
+            }
+            vm.statusClass = statusClass;
+        }
+
+        function makePayment() {
+            $state.go('app.transfers', {
+                toAccount: vm.loanAccountDetails
+            });
+        }
+    }
+})();
 (function () {
     'use strict';
     angular.module('selfService')
